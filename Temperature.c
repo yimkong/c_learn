b3f6708db98498135b67b15030b755ad82f06d3c
Ryan <yimkong1024@gmail.com>
2021-05-23 09:28:41 +0800

    add comment and gitinore file

--- Temperature.c
+++ Temperature.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
+/*
+当fahr = 0, 20, ··· , 300 时,分别打印华氏温度和摄氏温度对照表
+*/
 main(){
     int fahr, celsius;
     int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
+    lower = 0; // 温度表的下限
+    upper = 300; /* 温度表的上限 */
+    step = 20; // 步长
 
     fahr = lower;
     while(fahr <= upper){
